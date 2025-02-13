ff3cb1eedf8084b01d698d11eed4eb6508c9b1c7
Franco <elsieday72@gmail.com>
2025-02-13 17:02:59 -0500

    Correcin

--- script/main.js
+++ script/main.js
@@ -1,7 +1,9 @@
 // Animation Timeline
 const animationTimeline = () => {
-  const audio = new Audio("img/audio.mp3");
-audio.play();
+  
+
+ 
+  
   // Spit chars that needs to be animated individually
   const textBoxChars = document.getElementsByClassName("hbd-chatbox")[0];
   const hbd = document.getElementsByClassName("wish-hbd")[0];
@@ -270,13 +272,11 @@ audio.play();
   // tl.seek("currentStep");
   // tl.timeScale(2);
 
- // Botón para reiniciar animación y música
- const replyBtn = document.getElementById("replay");
- replyBtn.addEventListener("click", () => {
-   tl.restart();
-   audio.currentTime = 0; // Reinicia el audio
-   audio.play();
- });
+  // Restart Animation on click
+  const replyBtn = document.getElementById("replay");
+  replyBtn.addEventListener("click", () => {
+    tl.restart();
+  });
 };
 
 // Import the data to customize and insert them into page
